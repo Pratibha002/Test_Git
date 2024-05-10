80ac7d772c1a7eebd4c6e78c7f73a489b4aa375f
Pratibha <pratibhachouhan021@gmail.com>
2024-05-10 08:23:24 +0530

    first commit

---
+++ test.java
@@ -0,0 +1,155 @@
+package Test_Git;
+
+public class test {
+
+    public int pratibha() {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+
+        return c;
+
+    }
+
+    public short pratibha01() {
+        short a = 20;
+        short b = 30;
+        short c = (short) (a + b);
+
+        return c;
+    }
+
+    public byte pratibha02() {
+        byte a = 40;
+        byte b = 50;
+        byte c = (byte) (a + b);
+
+        return c;
+    }
+
+    public long pratibha03() {
+        int a = 60;
+        int b = 70;
+        int c = a + b;
+
+        return c;
+
+    }
+
+    public float pratibha04() {
+        float a = 10f;
+        float b = 20f;
+        float c = a + b;
+
+        return c;
+
+    }
+
+    public static double pratibha05() {
+        double a = 20;
+        double b = 40;
+        double c = a + b;
+
+        return c;
+
+    }
+
+    public static boolean pratibha06() {
+        return true;
+
+    }
+
+    public char pratibha07() {
+        char c = 'a';
+        return c;
+
+    }
+
+    public int pratibha08() {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+
+        return c;
+
+    }
+
+    public short pratibha09() {
+        byte a = 20;
+        byte b = 30;
+        byte c = (byte) (a + b);
+
+        return (short) c;
+    }
+
+    public byte pratibha10() {
+        int a = 10;
+        int b = 20;
+        int c = (a + b);
+
+        return (byte) c;
+
+    }
+
+    public long pratibha11() {
+        float a = 60;
+        float b = 70;
+        float c = a + b;
+
+        return (long) c;
+
+    }
+
+    public float pratibha12() {
+        int a = 60;
+        int b = 70;
+        int c = a + b;
+
+        return c;
+    }
+
+    public static double pratibha13() {
+        char c = 'a';
+        return c;
+
+    }
+
+    public static boolean pratibha14() {
+        return true;
+
+    }
+
+    public char pratibha15() {
+        double a = 20;
+        double b = 40;
+        double c = a + b;
+
+        return (char) c;
+
+    }
+
+    public static void main(String[] args) {
+        // return type
+
+        System.out.println("HELLO WORLD!!");
+        Test t = new Test();
+        System.out.println(t.pratibha());
+        System.out.println(t.pratibha01());
+        System.out.println(t.pratibha02());
+        System.out.println(t.pratibha03());
+        System.out.println(t.pratibha04());
+        System.out.println(test.pratibha05());
+        System.out.println(test.pratibha06());
+        System.out.println(t.pratibha07());
+        System.out.println(t.pratibha08());
+        System.out.println(t.pratibha09());
+        System.out.println(t.pratibha10());
+        System.out.println(t.pratibha11());
+        System.out.println(t.pratibha12());
+        System.out.println(test.pratibha13());
+        System.out.println(test.pratibha14());
+        System.out.println(t.pratibha15());
+
+    }
+
+}
